521c4b684877707523d00a83d7263d4302a62a0b
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-02-21 23:43:13 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var api = 'http://127.0.0.1:8004';
 var donationAddress = "RhodaLzk3r89PAUnB7NUTAEDfzY5ApiMRaL531j9SWqf3wMpGWiHckSMHHo5tNAoBB7cU9KTjxjRQG5ezFsf6CQY33fGtgWNEj";
-var blockTargetInterval = 30; // enter the block interval in seconds
+var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 rhox
 var totalSupply =  2100000000000000000; // enter the total supply in atomic units
 var symbol = 'rhox'; // enter the coin's ticker
@@ -9,7 +9,7 @@ var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
  "rhox": [
-	[rhox.cryptonote.club, "https://rhox.cryptonote.club/"],
+	[rhox.cryptonote.club, "https://rhox.cryptonote.club:8199/stats"],
 	//[roto2.org, "http://rhox.roto2.org/"],
 	//["us.turtlepool.space", "http://us.turtlepool.space:8117"],
 	//["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],

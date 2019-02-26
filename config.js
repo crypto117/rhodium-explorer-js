49eef9495ee5e132d94e5b10211aa85c924c995f
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-02-26 07:17:12 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-var api = 'https://rhox.cryptonote.club:8199/stats';
+var api = 'http://104.248.211.207';
 var donationAddress = "RhodaLzk3r89PAUnB7NUTAEDfzY5ApiMRaL531j9SWqf3wMpGWiHckSMHHo5tNAoBB7cU9KTjxjRQG5ezFsf6CQY33fGtgWNEj";
 var blockTargetInterval = 120; // enter the block interval in seconds
-var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 rhox
+var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 rhox
 var totalSupply =  2100000000000000000; // enter the total supply in atomic units
 var symbol = 'rhox'; // enter the coin's ticker
-var refreshDelay = 30000;
+var refreshDelay = 3000;
 
 // pools stats by MainCoins
 var networkStat = {

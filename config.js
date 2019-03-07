c420747532b9b75e6f0b83f622b3b1504eb37dcc
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-03-07 03:20:53 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://134.209.69.10:8010';
+var api = 'http://104.248.211.207:8010';
 var donationAddress = "RhodaLzk3r89PAUnB7NUTAEDfzY5ApiMRaL531j9SWqf3wMpGWiHckSMHHo5tNAoBB7cU9KTjxjRQG5ezFsf6CQY33fGtgWNEj";
 var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 rhox

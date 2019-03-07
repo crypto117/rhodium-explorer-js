24d246327b4897610fdd1496092aa4f6c4c8c949
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-03-07 02:12:30 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var api = 'http://104.248.211.207:8010';
+var api = 'http://134.209.69.10:8010';
 var donationAddress = "RhodaLzk3r89PAUnB7NUTAEDfzY5ApiMRaL531j9SWqf3wMpGWiHckSMHHo5tNAoBB7cU9KTjxjRQG5ezFsf6CQY33fGtgWNEj";
 var blockTargetInterval = 120; // enter the block interval in seconds
 var coinUnits = 100000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 rhox
@@ -10,18 +10,6 @@ var refreshDelay = 3000;
 var networkStat = {
  "rhox": [
 	[rhox.cryptonote.club, "https://rhox.cryptonote.club:8199/stats"],
-	//[roto2.org, "http://rhox.roto2.org/"],
-	//["us.turtlepool.space", "http://us.turtlepool.space:8117"],
-	//["hk.turtlepool.space", "http://hk.turtlepool.space:8117"],
-	//["turtlecoinpool.ddns.net", "http://turtlecoinpool.ddns.net:8127"],
-	//["trtl.mine2gether.com", "https://trtl.mine2gether.com/api"],
-	//["trtl.heigh-ho.funkypenguin.co.nz", "https://api.trtl.heigh-ho.funkypenguin.co.nz"],
-	//["trtl.radicaldelta.org", "http://trtl.radicaldelta.org:8117"],
-	//["turtle.atpool.party", "http://turtle-eu.atpool.party:8117"],
-	//["ny.minetrtl.us", "http://ny.minetrtl.us:8117"],
-	//["xk.is", "https://xk.is/api"],
-	//["trtl.llama.horse","http://pool.llama.horse:8118"],
-	//["trtl.hackerknowledge.de", "https://pool.trtl.hackerknowledge.de:8119"]
  ]
 };
 
